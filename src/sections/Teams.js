b9d16939968df76859d50f58072b96379927e0e2
luismariosanchez <luismariodsa@gmail.com>
2025-06-21 09:55:35 +0200

    feat(Teams): create basic team section

---
+++ src/sections/Teams.js
@@ -0,0 +1,10 @@
+
+export default function TeamsSection() {
+  return (
+    <section className="text-start pt-5">
+      <h1 className="text-4xl">
+        Equips Participants
+      </h1>
+    </section>
+  )
+}
